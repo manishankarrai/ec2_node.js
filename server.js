096c502130f18b61d2e0f91d6022d49c4c0d64b2
manishankarrai <manishankarray0@gmail.com>
2023-06-09 12:31:24 +0530

    updated version

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const helmet =  require('helmet');
 const app = express();
-const port = 3000 ;
+
 
 app.use(cors({
     origin: '*'
@@ -18,7 +18,6 @@ app.use(express.static('public'));
 
 app.get('/', (req, res) => res.sendFile('index.html'));
 
-let userdata = [] ;
 app.get('/getname' , (req,res)=>{
     let user = {
         name: "James Gun " ,
@@ -27,19 +26,8 @@ app.get('/getname' , (req,res)=>{
     res.send({message: "data come successfully ", data: user , value: true});
 
 });
-app.post('/postname' , (req,res)=> {
-   let user  = req.body ;
-   userdata.push(user);
-   res.send({message: "data post successfully " , data:  user , value: true});
-});
-
-app.get('/getalluser' , (req,res)=>{
-    res.send({ message : "success" , data:userdata , value: true});
-});
-app.get('/getreset', (req,res)=>{
-     userdata.length = 0 ;
-    res.send({message : "all data erased" ,data: userdata  , value: true})
-});
 
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
+
+app.listen(80);
